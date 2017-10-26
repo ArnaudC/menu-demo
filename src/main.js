853999c678889c63b37981470c1b002c418b6444
Arnaud Couchet <aco@softfluent.com>
2017-10-26 15:40:09 +0200

    Added a custom menu

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ const electron = require('electron')
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const {Menu} = electron
 
 app.on('ready', _ => {
     console.log('ready')    
     let win = new BrowserWindow({
         height: 400,
         width: 400})
+
+        const name = electron.app.getName();
+        const template = [{
+            label: name,
+            submenu: [{
+                label: `About ${name}`
+            }]
+        }]
+        const menu = Menu.buildFromTemplate(template)
+        Menu.setApplicationMenu(menu);
 })
 
